f3a206dce034e44ab07ee4eed3de0372fdbf2114
Ben <hostellerie.org@gmail.com>
2026-08-28 23:36:45 +0200

    Validate IndexNow 1.2.0 configuration

--- configuration_validation.php
+++ configuration_validation.php
@@ -2,29 +2,15 @@
 
 /* Reminder: always indent with 4 spaces (no tabs). */
 // +---------------------------------------------------------------------------+
-// | IndexNow Plugin 1.1.6                                                     |
+// | IndexNow Plugin 1.2.0                                                     |
 // +---------------------------------------------------------------------------+
 // | configuration_validation.php                                              |
-// |                                                                           |
-// | Validation functions for plugin configuration.                            |
-// +---------------------------------------------------------------------------+
-// | Copyright (C) 2024-2026 by the following authors:                         |
-// |                                                                           |
-// | Authors: Ben (hostellerie.org AT gmail DOT com)                           |
-// +---------------------------------------------------------------------------+
-// |                                                                           |
-// | This program is free software; you can redistribute it and/or             |
-// | modify it under the terms of the GNU General Public License               |
-// | as published by the Free Software Foundation; either version 2            |
-// | of the License, or (at your option) any later version.                    |
-// |                                                                           |
 // +---------------------------------------------------------------------------+
 
 if (strpos(strtolower($_SERVER['PHP_SELF']), 'configuration_validation.php') !== false) {
     die('This file cannot be used on its own!');
 }
 
-// Geeklog's configuration manager reads validation rules from this array.
 $_CONF_VALIDATE['indexnow']['debug_mode'] = array('rule' => 'boolean');
 
 ?>
